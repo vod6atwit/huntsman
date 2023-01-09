fabaa3d4f060c828102bf3f2621e67f5f78eb17b
Duyvo1117 <duyvo1117@gmail.com>
2023-01-09 13:41:39 -0500

    setup react router@6

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import Landing from './pages/Landing';
+import { Landing, Dashboard, Register, Error } from './pages';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Dashboard</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/landing">Landing</Link>
+      </nav>
+      <Routes>
+        {/* element can be html or components */}
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
+        {/* catch this route if all the routes above not match */}
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 import { Logo } from '../components';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
@@ -19,7 +20,9 @@ const Landing = () => {
             placeat quam. Iure molestias eius commodi velit odio? Error, labore
             distinctio. Eius.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
