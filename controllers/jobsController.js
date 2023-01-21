6890cbcd554b787884d120df330adc7a1db6937a
Duyvo1117 <duyvo1117@gmail.com>
2023-01-21 11:35:56 -0500

    completed showstats in jobsController

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -2,6 +2,7 @@ import Job from '../models/jobs.js';
 import { StatusCodes } from 'http-status-codes';
 import { CustomAPIError } from '../errors/index.js';
 import checkPermissions from '../utils/checkPermissions.js';
+import mongoose from 'mongoose';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -73,7 +74,26 @@ const deleteJob = async (req, res) => {
 };
 
 const showStats = async (req, res) => {
-  res.send('show stats');
+  let stats = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: '$status', count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplicationStats = [];
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplicationStats });
 };
 
 export { createJob, deleteJob, getAllJobs, editJob, showStats };
